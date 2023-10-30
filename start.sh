02630ff561d04bfa116737bc1a45655bf17d2e2a
Joshua Dotto <joshdotto85.github@gmail.com>
2023-10-29 23:11:53 -0500

    add udev rules

--- start.sh
+++ start.sh
@@ -6,5 +6,9 @@
 # This file serves as the entry point for 
 # the night-monitor project
 
+curl -fsSL https://raw.githubusercontent.com/platformio/platformio-core/develop/platformio/assets/system/99-platformio-udev.rules | sudo tee /etc/udev/rules.d/99-platformio-udev.rules
+sudo service udev restart
+sudo usermod -a -G dialout $USER
+sudo usermod -a -G plugdev $USER
 cd end-device && pio run -t upload
 cd ../gateway && python3 main.py
